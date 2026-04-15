9521e7da06b225f5888fb71461a11c9a4b388c4d
sharafath ali <sharafathaliofficial@gmail.com>
2026-04-15 16:49:21 +0530

    add square pattern printing function

--- pattern-printing.js
+++ pattern-printing.js
@@ -13,3 +13,17 @@ function printLeftAlignedRightTriangle(num) {
 
 printLeftAlignedRightTriangle(7)
 
+
+console.log("print sqaure")
+
+function printsqaure(num){
+  for (var i = 1; i <= num; i++) {
+    var row = "";
+    for (var j = 1; j <= num; j++) {
+      row = row + "*"
+    }
+    console.log(row)
+  }
+}
+
+printsqaure(4)
